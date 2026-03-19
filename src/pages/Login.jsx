2e452ba15d33db5d2a8a79db41e0e012899765ca
Gerson Colmena <gersonmamani01@gmail.com>
2026-03-18 22:49:28 -0400

    Estructura inicial del proyecto POS con React y Vite

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+import { Link } from 'react-router-dom';
+import LoginPageTemplate from '../components/login/LoginPage';
+
+
+export default function Login() {
+    return (
+            
+            <div className="flex h-screen w-full items-center justify-center bg-gray-800 p-4">
+                <LoginPageTemplate />
+            </div>
+    );
+}
